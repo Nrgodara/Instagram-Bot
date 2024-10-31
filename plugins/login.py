5b19883753c64c9fb81b1c127a9d4b549f9e362c
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-10-31 20:17:12 +0530

    Update login.py

--- plugins/login.py
+++ plugins/login.py
@@ -105,7 +105,12 @@ async def login(bot, message):
         name = profile.full_name
         bio = profile.biography
         profilepic = profile.profile_pic_url
-        igtvcount = profile.igtvcount
+        #igtvcount = profile.igtvcount
+	try:
+            igtvcount = profile.igtvcount
+        except KeyError:
+            igtvcount = 0  # or any other fallback value you prefer
+
         followers = profile.followers
         following = profile.followees
         await m.delete()
